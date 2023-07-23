1a2f9bdb32461acab7705035a422dc25810eb4d1
carlynorama <carlynorama@gmail.com>
2023-07-22 18:06:59 -0700

    add usdcat

--- Sources/USDServiceProvider/USDServiceProvider.swift
+++ Sources/USDServiceProvider/USDServiceProvider.swift
@@ -1,20 +1,24 @@
 import Foundation
 
 public struct USDServiceProvider {
-    public private(set) var text = "Hello, World!"
+    public private(set) var pathToBin:String
+    public private(set) var pythonPath:String
     
-    public init() {
+    public init(_ pathToUSD:String? = nil) {
+        self.pathToBin = "/Users/carlynorama/opd/USD_nousdview_0722/bin"
+        self.pythonPath = "/Users/carlynorama/opd/USD_nousdview_0722/lib/python"
     }
     
     
-    public func echo(_ input:String) -> String {
-        if input.isEmpty {
-            return "[crickets chirping]"
-        } else {
-            let message = try? shell("echo \(input)")
-            return (message != nil) ? message! : "nothing to say"
-        }
-        
+    public func usdcatHelp() -> String {
+        let message = try? shell("\(pathToBin)/usdcat -h")
+        return (message != nil) ? message! : "nothing to say"
+    }
+    
+    public func makeUSDC(inputFile:String, outputFile:String) {
+       // print(try? shell("pwd"))
+        let message = try? shell("\(pathToBin)/usdcat -o \(outputFile) --flatten \(inputFile)")
+        print(message ?? "")
     }
     
     @discardableResult // Add to suppress warnings when you don't want/need a result
@@ -28,6 +32,13 @@ public struct USDServiceProvider {
         
         task.standardInput = nil
         task.executableURL = URL(fileURLWithPath: "/bin/bash") //<-- what shell
+        
+        
+//        var environment =  ProcessInfo.processInfo.environment
+//        environment["PYTHONPATH"] = "\(pythonPath)"
+        //task.environment = environment
+        print(task.environment ?? "")
+        
         try task.run()
         
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
@@ -35,18 +46,41 @@ public struct USDServiceProvider {
         
         return output
     }
-    
-    public func whatsInMyBin() {
-        let testPath = "/usr/bin"
-        let ls = Process()
-        //https://manned.org/env.1
-        ls.executableURL = URL(fileURLWithPath: "/usr/bin/env")
-        ls.arguments = ["ls", "-al", testPath]
-        do{
-            try ls.run()
-        } catch {
-            print(error)
-        }
-    }
-    
 }
+
+extension USDServiceProvider {
+    //TODO: Needs improvement
+//    public init?() {
+//        guard let path = try? Self.shell("which usdchecker") else {
+//            return nil
+//        }
+//        if path.isEmpty { return nil }
+//        if path.contains("not found") { return nil }
+//        guard let urlTest = URL(string: path ) else { return nil }
+//        self.pathToBin = path
+//    }
+}
+
+
+//public func echo(_ input:String) -> String {
+//    if input.isEmpty {
+//        return "[crickets chirping]"
+//    } else {
+//        let message = try? shell("echo \(input)")
+//        return (message != nil) ? message! : "nothing to say"
+//    }
+//
+//}
+//
+//public func whatsInMyBin() {
+//    let testPath = "/usr/bin"
+//    let ls = Process()
+//    //https://manned.org/env.1
+//    ls.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+//    ls.arguments = ["ls", "-al", testPath]
+//    do{
+//        try ls.run()
+//    } catch {
+//        print(error)
+//    }
+//}
